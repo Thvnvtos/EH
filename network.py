33f1aaf51b0fc1e6ca35cc5228e55e7e7d9bc85b
Ilyass Hammouamri <hammouamri.ilyass@gmail.com>
2025-04-11 12:10:32 +0200

    hw2

--- network.py
+++ network.py
@@ -76,7 +76,7 @@ class NetCNN2D_CSP(keras.Model):
 
         self.cnn1 = keras.Sequential(
             [
-                keras.layers.Conv2D(filters=8, kernel_size=(11,11), strides=(7,7), padding='same'),
+                keras.layers.Conv2D(filters=8, kernel_size=(7,7), strides=(7,7), padding='same'),
                 keras.layers.BatchNormalization(),
                 keras.layers.ReLU(max_value=1000.0),
 
@@ -84,7 +84,7 @@ class NetCNN2D_CSP(keras.Model):
 
         self.cnn2 = keras.Sequential(
             [
-                keras.layers.Conv2D(filters=16, kernel_size=(7,7), strides=(5,5), padding='same'),
+                keras.layers.Conv2D(filters=16, kernel_size=(5,5), strides=(5,5), padding='same'),
                 keras.layers.BatchNormalization(),
                 keras.layers.ReLU(max_value=1000.0)
             ], name='cnn2')
@@ -128,12 +128,12 @@ def build_functional_cnn2D(n_classes, input_shape):
     inputs = tf.keras.Input(shape=input_shape)
     
     # cnn1
-    x = tf.keras.layers.Conv2D(filters=8, kernel_size=(11, 11), strides=(7, 7), padding='same')(inputs)
+    x = tf.keras.layers.Conv2D(filters=8, kernel_size=(7, 7), strides=(7, 7), padding='same')(inputs)
     x = tf.keras.layers.BatchNormalization()(x)
     x = tf.keras.layers.ReLU(max_value=1000.0)(x)
 
     # cnn2
-    x = tf.keras.layers.Conv2D(filters=16, kernel_size=(7, 7), strides=(5, 5), padding='same')(x)
+    x = tf.keras.layers.Conv2D(filters=16, kernel_size=(5, 5), strides=(5, 5), padding='same')(x)
     x = tf.keras.layers.BatchNormalization()(x)
     x = tf.keras.layers.ReLU(max_value=1000.0)(x)
     
