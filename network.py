fad5128b51f665c75b28136b6ea2a1cbd686729b
Ilyass Hammouamri <hammouamri.ilyass@gmail.com>
2025-04-11 12:22:01 +0200

    hw3

--- network.py
+++ network.py
@@ -66,7 +66,8 @@ class NetCNN2D_CSP_GAP(keras.Model):
 
 
 
-
+# Kernel size should be 3, 5, 7 and symmetric
+# strides should be 1, 2 or 3
 
 class NetCNN2D_CSP(keras.Model):
     def __init__(self, n_classes, **kwargs):
@@ -76,7 +77,7 @@ class NetCNN2D_CSP(keras.Model):
 
         self.cnn1 = keras.Sequential(
             [
-                keras.layers.Conv2D(filters=8, kernel_size=(7,7), strides=(7,7), padding='same'),
+                keras.layers.Conv2D(filters=8, kernel_size=(7,7), strides=(3,3), padding='same'),
                 keras.layers.BatchNormalization(),
                 keras.layers.ReLU(max_value=1000.0),
 
@@ -84,7 +85,15 @@ class NetCNN2D_CSP(keras.Model):
 
         self.cnn2 = keras.Sequential(
             [
-                keras.layers.Conv2D(filters=16, kernel_size=(5,5), strides=(5,5), padding='same'),
+                keras.layers.Conv2D(filters=16, kernel_size=(7,7), strides=(3,3), padding='same'),
+                keras.layers.BatchNormalization(),
+                keras.layers.ReLU(max_value=1000.0)
+            ], name='cnn2')
+        
+
+        self.cnn3 = keras.Sequential(
+            [
+                keras.layers.Conv2D(filters=32, kernel_size=(5,5), strides=(3,3), padding='same'),
                 keras.layers.BatchNormalization(),
                 keras.layers.ReLU(max_value=1000.0)
             ], name='cnn2')
@@ -105,6 +114,7 @@ class NetCNN2D_CSP(keras.Model):
     def call(self, x):
         temp =self.cnn1(x)
         temp = self.cnn2(temp)
+        temp = self.cnn3(temp)
         temp = self.fc(temp)
         
         return temp
@@ -128,12 +138,17 @@ def build_functional_cnn2D(n_classes, input_shape):
     inputs = tf.keras.Input(shape=input_shape)
     
     # cnn1
-    x = tf.keras.layers.Conv2D(filters=8, kernel_size=(7, 7), strides=(7, 7), padding='same')(inputs)
+    x = tf.keras.layers.Conv2D(filters=8, kernel_size=(7, 7), strides=(3, 3), padding='same')(inputs)
     x = tf.keras.layers.BatchNormalization()(x)
     x = tf.keras.layers.ReLU(max_value=1000.0)(x)
 
     # cnn2
-    x = tf.keras.layers.Conv2D(filters=16, kernel_size=(5, 5), strides=(5, 5), padding='same')(x)
+    x = tf.keras.layers.Conv2D(filters=16, kernel_size=(7, 7), strides=(3, 3), padding='same')(x)
+    x = tf.keras.layers.BatchNormalization()(x)
+    x = tf.keras.layers.ReLU(max_value=1000.0)(x)
+
+    # cnn3
+    x = tf.keras.layers.Conv2D(filters=32, kernel_size=(5, 5), strides=(3, 3), padding='same')(x)
     x = tf.keras.layers.BatchNormalization()(x)
     x = tf.keras.layers.ReLU(max_value=1000.0)(x)
     
