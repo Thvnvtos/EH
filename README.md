5c4904057138f44fcbf84ef10eea3595159d1662
Ilyass Hammouamri <hammouamri.ilyass@gmail.com>
2025-04-16 11:14:13 +0100

    network.py completed

---
+++ README.md
@@ -0,0 +1,5 @@
+# Kernel size should be 3, 5, 7 and symmetric
+# strides should be 1, 2 or 3 symmetric (input) only 1,2 intermediary
+# Input dimension cannot be smaller than 5
+# Max input size is 256
+# convolution stride 2 only supported for 3x3
--- network.py
+++ network.py
@@ -1,12 +1,36 @@
+"""
+networks.py
+
+This module contains the definition of Keras CNN models used with both CPU/GPU and Akida. 
+
+Notes:
+    - Model definition could be improved by parameterization to easily try different model configurations
+    - Removing the separate functional definition of the Akida model
+"""
+
+
+
 from tensorflow import keras 
 import tensorflow as tf
 
 
-class NetCNN2D_CSP_GAP(keras.Model):
+class CPU_NetCNN2D_CSP(keras.Model):
+    """
+    A simple 2D CNN used after the CSP transformation. This version is designed for CPU/GPU not for Akida. 
+    Since Global Average Pooling is not compatible with Akida, but it seems useful for generalization.
+    
+    Parameters
+    ----------
+    n_classes : int
+        Number of output classes for classification
+    """
+
+
     def __init__(self, n_classes, **kwargs):
         super().__init__(**kwargs)
 
         self.n_classes = n_classes
+
 
         self.cnn1 = keras.Sequential(
             [
@@ -41,7 +65,6 @@ class NetCNN2D_CSP_GAP(keras.Model):
             ], name = 'fc')
         
 
-
     def call(self, x):
         temp =self.cnn1(x)
         temp = self.cnn2(temp)
@@ -51,6 +74,7 @@ class NetCNN2D_CSP_GAP(keras.Model):
         return temp
     
 
+    # Re-define get_config and from_config for the serialized saving of the models.
     def get_config(self):
         config = super().get_config()
         config.update({
@@ -66,13 +90,23 @@ class NetCNN2D_CSP_GAP(keras.Model):
 
 
 
-# Kernel size should be 3, 5, 7 and symmetric
-# strides should be 1, 2 or 3 symmetric (input) only 1,2 intermediary
-# Input dimension cannot be smaller than 5
-# Max input size is 256
-# convolution stride 2 only supported for 3x3
 
-class NetCNN2D_CSP(keras.Model):
+
+class Akida_HWOnly_NetCNN2D_CSP(keras.Model):
+    """
+    A simple 2D CNN used after the CSP transformation.
+
+    This version is designed for Akida Hardware Only mode (hw_only = True)
+    (See list of hw_only mode constraints in README) 
+    
+    
+    Parameters
+    ----------
+    n_classes : int
+        Number of output classes for classification
+    """
+
+
     def __init__(self, n_classes, **kwargs):
         super().__init__(**kwargs)
 
@@ -121,7 +155,8 @@ class NetCNN2D_CSP(keras.Model):
         
         return temp
     
-
+    # Re-define get_config and from_config for the serialized saving of the models.
+    # Might be not needed here, as the akida package will be in charge of saving these models
     def get_config(self):
         config = super().get_config()
         config.update({
@@ -136,7 +171,29 @@ class NetCNN2D_CSP(keras.Model):
 
 
 
-def build_functional_cnn2D(n_classes, input_shape):
+def build_functional_cnn2D(n_classes, input_shape = (7, 250, 1)):
+    """
+    convert the model to a functional form for Quantization/akida conversion.
+    Needs to be changed according to changes in the original model above.
+
+    Note: the model could be defined by default in this form.
+
+    Parameters
+    ----------
+    n_classes : int
+        Number of output classes for classification
+    input_shape: tuple
+        The input shape of the model: (Spatial_dimension, Time_dimension, channels), 
+        defaults to (7, 250, 1): 
+            7 is 3 CSP channels + zero padding for kernel_size=7
+            250 is 1 second slice (sampling rate = 250)
+            1 is the number of channels
+    
+    Returns
+    -------
+    functional_model
+    
+    """
     inputs = tf.keras.Input(shape=input_shape)
     
     # cnn1
@@ -163,69 +220,3 @@ def build_functional_cnn2D(n_classes, input_shape):
     
     functional_model = tf.keras.Model(inputs=inputs, outputs=outputs)
     return functional_model
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-class NetCNN1D(keras.Model):
-    '''
-        Simple conv model for testing purposes
-    '''
-    def __init__(self, n_classes, **kwargs):
-        super().__init__(**kwargs)
-
-
-
-        self.cnn1 = keras.Sequential(
-            [
-                keras.layers.Conv1D(filters=12, kernel_size=11, strides=7, groups=3),
-                keras.layers.BatchNormalization(),
-                keras.layers.ReLU(max_value=1000.0),
-            ], name='cnn1')
-
-        self.cnn2 = keras.Sequential(
-            [
-
-                keras.layers.Conv1D(filters=18, kernel_size=7, strides=5, groups=3),
-                keras.layers.BatchNormalization(),
-                keras.layers.ReLU(max_value=1000.0),
-            ], name='cnn2')
-
-        self.gap = keras.Sequential(
-            [
-                keras.layers.GlobalAveragePooling1D(), 
-            ]
-        )
-
-        self.fc = keras.Sequential(
-            [
-                keras.layers.Flatten(),
-                keras.layers.Dense(128),
-                keras.layers.ReLU(max_value=1000.0),
-                keras.layers.Dropout(0.1),
-                keras.layers.Dense(n_classes)
-
-            ]
-        , name = 'fc')
-
-
-
-    def call(self, x):
-        temp = self.cnn1(x)
-        temp = self.cnn2(temp)
-        temp = self.gap(temp)
-        temp = self.fc(temp)
-        return temp
